5b14b6bb562468e3c74c55250d5c7f96a6dd3ab6
Mahbod6968df <mahbodiwe@atomicmail.io>
2026-09-04 22:02:42 +0330

    Update entrypoint.sh

--- New folder/entrypoint.sh
+++ New folder/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# جایگذاری متغیر PORT در کانفیگ Caddy
-envsubst '${PORT}' < /etc/caddy/Caddyfile.template > /etc/caddy/Caddyfile
-
 # اجرای پنل سنایی در پس‌زمینه
 /app/x-ui &
 
-# اجرای وب‌سرور Caddy در پیش‌زمینه
+# اجرای وب‌سرور Caddy
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
